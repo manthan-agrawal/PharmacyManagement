582fdeb51766bea069444928f8637a6641c6c15f
meetkakadiya44 <47652924+meetkakadiya44@users.noreply.github.com>
2019-11-28 16:18:34 +0530

    added signup template

--- ProjectFiles/PharmacyManagement/src/javaapplication/login.java
+++ ProjectFiles/PharmacyManagement/src/javaapplication/login.java
@@ -151,6 +151,9 @@ public class login extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+        
+        Signup main_obj=new Signup();
+        main_obj.setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void pflpassActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pflpassActionPerformed
